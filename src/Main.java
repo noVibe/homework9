d52616e285fb684477f745d0da4b2fe608d65702
Василий <remindscope@gmail.com>
2022-11-29 18:43:00 +0300

    Task 3 refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-//   =====Task 1, 2 3combined=====
-
+//      =====Task 1=====
         int[] firstArray = new int[3];
         for (int i = 0; i < firstArray.length; i++) {
             firstArray[i] = i + 1;
         }
         double[] secondArray = {1.57, 7.654, 9.986};
         boolean[] thirdArray = {true, false, false};
-        System.out.println("=====Task 1=====");
+
+//      =====Task 2=====
+        System.out.println("=====Task 2=====");
         System.out.printf("%s \n%s \n%s \n", Arrays.toString(firstArray), Arrays.toString(secondArray), Arrays.toString(thirdArray));
 
-        System.out.println("Inverted:");
-
+//      =====Task 3=====
+        System.out.println("=====Task 3=====");
         String arrayString = Arrays.toString(firstArray);
         inversionForAnyArray(arrayString);
-
         arrayString = Arrays.toString(secondArray);
         inversionForAnyArray(arrayString);
-
         arrayString = Arrays.toString(thirdArray);
         inversionForAnyArray(arrayString);
 
@@ -38,21 +38,9 @@ public class Main {
     }
     // Method of Task 3
     private static void inversionForAnyArray(String arrayString) {
-        StringBuilder result = new StringBuilder();
         String symbolsRemoved = arrayString.replace(",", "").replace("]", "").replace("[", "");
         String[] arrayStringSplit = symbolsRemoved.split("\\s");
-        for (int i = arrayStringSplit.length - 1; i >=0 ; i--) {
-            if (i == arrayStringSplit.length - 1){
-                result.append("[");
-            }
-            result.append(arrayStringSplit[i]);
-            if (i == 0) {
-                result.append("]");
-                break;
-            }
-            result.append(", ");
-        }
-        System.out.println(result);
-
+        Collections.reverse(Arrays.asList(arrayStringSplit));
+        System.out.println(Arrays.asList(arrayStringSplit));
     }
 }
